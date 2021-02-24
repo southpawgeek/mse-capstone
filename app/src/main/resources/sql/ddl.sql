f788760e0cd472e9fc3bdfc34a72db662952fc0e
Jen Guerra <jag@brandeis.edu>
2021-02-23 19:19:01 -0500

    update config to use AWS database

--- app/src/main/resources/sql/ddl.sql
+++ app/src/main/resources/sql/ddl.sql
@@ -1,7 +1,7 @@
-create user if not exists 'lms'@'localhost' identified by 'rseg127';
+create user if not exists 'lms'@'%' identified by 'rseg127';
 
 create database if not exists lms;
 use lms;
-grant all privileges on lms.* to 'lms'@'localhost';
+grant all privileges on lms.* to 'lms'@'%';
 flush privileges;
 set global time_zone = '+5:00';
