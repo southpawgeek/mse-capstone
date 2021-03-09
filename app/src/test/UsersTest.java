ecaa001add468be9fa418b30a3d7a2ad3cca1932
adamsher@brandeis.edu <A1baethus>
2021-03-08 21:29:10 -0500

    Created outline for UsersTest

---
+++ app/src/test/UsersTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import edu.brandeis.gps.rseg127.lms.entities.User;
+import edu.brandeis.gps.rseg127.lms.services.UserService;
+
+public class UsersTest{
+    public static void main (String[]args)
+    {
+        System.out.println("Creating test user" + user);
+    }
+
+    @Test
+    public int testAddUser ()
+    {
+        User user = new User();
+
+        return 1;
+    }
+}
