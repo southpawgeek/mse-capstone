bb1c3e381d76c4de79a5134dcdc42cde6c79ea14
Jen Guerra <jag@brandeis.edu>
2021-03-22 13:22:59 -0400

    remove unused import

--- app/src/main/java/edu/brandeis/gps/rseg127/lms/entities/User.java
+++ app/src/main/java/edu/brandeis/gps/rseg127/lms/entities/User.java
@@ -8,12 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class User {
 
