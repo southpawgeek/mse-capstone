c4a20babb58a83e224ae0fd1ff7149a42397f188
Jen Guerra <jag@brandeis.edu>
2021-03-22 23:51:38 -0400

    add view, add cleanup triggers for deletes

--- app/src/main/resources/sql/schema.sql
+++ app/src/main/resources/sql/schema.sql
@@ -17,10 +17,11 @@ create table user(
     primary key (id)
 );
 
-create table cart(
+create table asset(
     id int not null auto_increment,
-    user_id int not null,
-    copy_id int not null,
+    title varchar(100) not null,
+    isbn varchar(50) not null,
+    call_number varchar(50),
     primary key (id)
 );
 
@@ -33,18 +34,11 @@ create table asset_copy(
     primary key (id)
 );
 
-create table asset(
+create table cart(
     id int not null auto_increment,
-    title varchar(100) not null,
-    isbn varchar(50) not null,
-    call_number varchar(50),
+    user_id int not null,
+    copy_id int not null,
     primary key (id)
-);
-
-create table asset_author(
-    asset_id int not null,
-    author_id int not null,
-    primary key (asset_id, author_id)
 );
 
 create table author(
@@ -53,6 +47,12 @@ create table author(
     middle_name varchar(75),
     last_name varchar(75),
     primary key (id)
+);
+
+create table asset_author(
+    asset_id int not null,
+    author_id int not null,
+    primary key (asset_id, author_id)
 );
 
 create table audit(
@@ -64,3 +64,36 @@ create table audit(
     action_date timestamp not null default current_timestamp,
     primary key (id)
 );
+
+create view copy_asset_user_vw as 
+select a.id as asset_id, a.title, a.isbn, u.id as user_id, u.username, ac.id as copy_id, ac.status, ac.due_date 
+from asset_copy ac 
+left join asset a on ac.asset_id = a.id 
+left join user u on ac.user_id = u.id;
+
+delimiter |
+create trigger copy_cleanup_on_asset_delete 
+after delete on asset
+for each row
+begin
+    delete from asset_copy where asset_copy.asset_id = old.id;
+end
+|
+
+create trigger cart_cleanup_on_copy_delete 
+after delete on asset_copy 
+for each row
+begin
+    delete from cart where cart.copy_id = old.id;
+end
+|
+
+create trigger cart_cleanup_on_user_delete 
+after delete on user
+for each row
+begin
+    delete from cart where cart.user_id = old.id;
+end
+|
+
+delimiter ;
