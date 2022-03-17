aa1ced954fcee32a87e40c821fe6fd16f4be8526
Jen Guerra <jen@southpawgeek.com>
2022-03-17 18:03:57 -0400

    update example db user to localhost

--- app/src/main/resources/sql/ddl.sql
+++ app/src/main/resources/sql/ddl.sql
@@ -1,9 +1,9 @@
-create user if not exists 'lms'@'%' identified by 'rseg127';
+create user if not exists 'lms'@'localhost' identified by 'rseg127';
 
 create database if not exists lms;
 use lms;
-grant all privileges on lms.* to 'lms'@'%';
+grant all privileges on lms.* to 'lms'@'localhost';
 flush privileges;
 
--- EDT, since it's march
-set global time_zone = '-4:00';
+-- EST
+set global time_zone = '-5:00';
